da1548c7ce5d68587edbe520f3e97944b99c563d
Victor <chrollolucilfer1402@gmail.com>
2023-06-08 11:59:25 +0530

    learned crud operations in mongodb and node

--- mongo-basics/src/crud.js
+++ mongo-basics/src/crud.js
@@ -1,5 +1,5 @@
 import { MongoClient } from "mongodb";
-// import { URI } from "../src/env.js";
+import { players as playerArray } from "./data.js";
 
 const URI = "mongodb://localhost:27017";
 const doc = { _id: crypto.randomUUID(), name: "Pogba", country: "France" };
@@ -14,7 +14,7 @@ const create = async () => {
 
     const database = client.db("Football");
     const players = database.collection("players");
-    const result = await players.insertOne(doc, { forceServerObjectId: true });
+    const result = await players.insertOne(doc);
     console.log(`A document was inserted with id = ${result.insertedId}`);
   } catch (error) {
     console.log(error);
@@ -23,4 +23,94 @@ const create = async () => {
   }
 };
 
-create().catch(console.dir);
+// insertMany
+const createMany = async () => {
+  try {
+    await client.connect();
+
+    const cName = client.db("Football").collection("players");
+    let result = await cName.insertMany(playerArray);
+    console.log(`${result.insertedCount} documents were inserted `);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await client.close();
+  }
+};
+
+// query types
+const literalQuery = { country: "India" }; // literal
+const compareQuery = { goals: { $gt: 500 } }; // comparison
+const logicQuery = {
+  $and: [
+    { goals: { $lt: 500 } },
+    { netWorth: { $not: { $eq: "$100 million" } } },
+  ],
+};
+const elementQuery = { country: { $exists: true } };
+const evalQuery = { name: { $regex: "L*sM*" } };
+
+// Reading from a database collection
+async function read() {
+  await client.connect();
+  const myColl = client.db("Football").collection("players");
+  // literal value queries
+  const cursor = myColl.find(elementQuery);
+  for await (const doc of cursor) {
+    console.dir(doc);
+  }
+}
+
+// Delete operations
+async function Delete() {
+  try {
+    await client.connect();
+
+    const myColl = client.db("Football").collection("players");
+    const result = await myColl.deleteMany();
+    console.log(`Deleted ${result.deletedCount} documents`);
+  } catch (error) {
+    console, log(error);
+  } finally {
+    client.close();
+  }
+}
+
+const filter = { name: "Neymar Jr" };
+const updateGoals = {
+  $set: {
+    goals: 490,
+    trophies: 23,
+  },
+};
+
+const replaceDoc = {
+  name: "Neymar Jr",
+  country: "Brazil",
+  goals: 436,
+  trophies: 23,
+  netWorth: "$200 million",
+};
+
+const Update = async () => {
+  try {
+    await client.connect();
+
+    const database = client.db("Football");
+    const myColl = database.collection("players");
+    // const result = await myColl.updateOne(filter, updateGoals);
+    const result = await myColl.replaceOne(filter, replaceDoc);
+    console.log(`${result.modifiedCount} documents modified`);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    client.close();
+  }
+};
+
+// create().catch(console.dir);
+// createMany().catch(console.error);
+// read();
+// Delete().catch(console.error);
+
+Update();
---
+++ mongo-basics/src/data.js
@@ -0,0 +1,37 @@
+export const players = [
+  {
+    name: "Cristiano Ronaldo",
+    country: "Portugal",
+    goals: 674,
+    trophies: 34,
+    netWorth: "$500 million",
+  },
+  {
+    name: "Lionel Messi",
+    country: "Argentina",
+    goals: 672,
+    trophies: 36,
+    netWorth: "$400 million",
+  },
+  {
+    name: "Neymar Jr",
+    country: "Brazil",
+    goals: 249,
+    trophies: 23,
+    netWorth: "$200 million",
+  },
+  {
+    name: "Robert Lewandowski",
+    country: "Poland",
+    goals: 297,
+    trophies: 22,
+    netWorth: "$120 million",
+  },
+  {
+    name: "Kylian Mbappe",
+    country: "France",
+    goals: 131,
+    trophies: 11,
+    netWorth: "$150 million",
+  },
+];
